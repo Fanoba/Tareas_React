1a2ec7b926fdf5cd09b821e599ac8547eb864263
Gilberto Angel Camacho Lara <135077570+Fanoba@users.noreply.github.com>
2025-02-24 22:15:43 -0600

    Actualizado

--- mi-proyecto/src/components/SampleForm.jsx
+++ mi-proyecto/src/components/SampleForm.jsx
@@ -1,13 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Message } from './Message';
 
 export const SampleForm = () => {
     const [formState, setFormState] = useState({
-        username: 'strider',
-        email: 'fernando@google.com'
+        Matricula: '',
+        Nombre: '',
+        ApellidoP: '',
+        ApellidoM: '',
+        Edad: 0,
+        Carrera: ''
     });
 
-    const { username, email } = formState;
+    const [submitted, setSubmitted] = useState(false);
+
+    const { Matricula, Nombre, ApellidoP, ApellidoM, Edad, Carrera } = formState;
 
     const onInputChange = ({ target }) => {
         const { name, value } = target;
@@ -17,46 +23,93 @@ export const SampleForm = () => {
         });
     };
 
-    useEffect(() => {
-        console.log('useEffect inicial ejecutado');
-    }, []);
-
-    useEffect(() => {
-        console.log('formState cambió:', formState);
-    }, [formState]);
-
-    useEffect(() => {
-        console.log('email cambió:', email);
-    }, [email]);
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        setSubmitted(true); 
+    };
 
     return (
         <>
             <h1>Formulario Simple</h1>
             <hr />
-
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input 
-                type="text" 
-                className="form-control" 
-                placeholder="Username" 
-                name="username"
-                value={username}
-                onChange={onInputChange}
-                autoComplete="off"
-            />
+                    type="text" 
+                    className="form-control" 
+                    placeholder="Matrícula" 
+                    name="Matricula"
+                    value={Matricula}
+                    onChange={onInputChange}
+                    autoComplete="off"
+                />
 
-            <input 
-                type="email" 
-                className="form-control mt-2" 
-                placeholder="jorge@gmail.com" 
-                name="email"
-                value={email}
-                onChange={onInputChange}
-                autoComplete="off"
-            />
-        </form>
+                <input 
+                    type="text" 
+                    className="form-control mt-2" 
+                    placeholder="Nombre" 
+                    name="Nombre"
+                    value={Nombre}
+                    onChange={onInputChange}
+                    autoComplete="off"
+                />
 
-        {username === 'strider2' && <Message />}
-    </>
-    );
+                <input 
+                    type="text" 
+                    className="form-control mt-2" 
+                    placeholder="Apellido Paterno" 
+                    name="ApellidoP"
+                    value={ApellidoP}
+                    onChange={onInputChange}
+                    autoComplete="off"
+                />
+
+                <input 
+                    type="text" 
+                    className="form-control mt-2" 
+                    placeholder="Apellido Materno" 
+                    name="ApellidoM"
+                    value={ApellidoM}
+                    onChange={onInputChange}
+                    autoComplete="off"
+                />
+
+                <input 
+                    type="number" 
+                    className="form-control mt-2" 
+                    placeholder="Edad" 
+                    name="Edad"
+                    value={Edad}
+                    onChange={onInputChange}
+                    autoComplete="off"
+                />
+
+                <input 
+                    type="text" 
+                    className="form-control mt-2" 
+                    placeholder="Carrera" 
+                    name="Carrera"
+                    value={Carrera}
+                    onChange={onInputChange}
+                    autoComplete="off"
+                />
+
+                <button type="submit">Enviar</button>
+            </form>
+
+        
+            {submitted && (
+                <div className="info-container">
+                    <p>Matrícula: {Matricula}</p>
+                    <p>Nombre: {Nombre}</p>
+                    <p>Apellido Paterno: {ApellidoP}</p>
+                    <p>Apellido Materno: {ApellidoM}</p>
+                    <p>Edad: {Edad}</p>
+                    <p>Carrera: {Carrera}</p>
+                </div>
+            )}
+
+         
+            
+        </>
+    );
 };
